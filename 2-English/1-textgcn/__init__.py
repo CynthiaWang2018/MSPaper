a3af2c6d9cbd66f7ca4dcef01c091de60172a57d
cynthyzwang <wangzhaoonly@163.com>
2020-03-09 13:27:18 +0800

    update5

--- 2-English/1-textgcn/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function
-# 即使在python2.X, 使用print函数也同python3.X相同, 即加括号使用
-from __future__ import division
-# 导入python未来支持的语言特征division(精确除法)
-# 当没有在程序中导入该特征时, "/"操作符执行的是截断除法(Truncating Division)
-# 当导入精确除法后, "/"执行精确除法操作, "//"执行截断除法
